a64cce3e1634fca522e27bfcdbbb512681e337ce
masato-ka <nsb21708@nifty.com>
2017-03-18 12:43:21 +0900

    Add setting for ffmpeg binary paths.

--- src/main/java/com/masato/ka/hls/config/FFmpegConfiguration.java
+++ src/main/java/com/masato/ka/hls/config/FFmpegConfiguration.java
@@ -2,6 +2,7 @@ package com.masato.ka.hls.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -13,14 +14,19 @@ import net.bramp.ffmpeg.FFprobe;
 @Configuration
 public class FFmpegConfiguration {
 
+	@Value("{ffmpeg.ffmpeg:/usr/local/bin/ffmpeg}")
+	private String ffmpegBinaryPath;
+	@Value("{ffmpeg.ffprobe:/usr/local/bin/ffprobe}")
+	private String ffprobeBinaryPath;
+	
     @Bean
     public FFmpeg ffmpeg() throws IOException {
-        return new FFmpeg("/usr/local/bin/ffmpeg");
+        return new FFmpeg(ffmpegBinaryPath);
     }
     
     @Bean
     public FFprobe ffprobe() throws IOException {
-        return new FFprobe("/usr/local/bin/ffprobe");
+        return new FFprobe(ffprobeBinaryPath);
     }
     
     @Bean
